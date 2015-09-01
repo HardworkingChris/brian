5eccb3fd55e597a0d0bd0c1354f911e03d93b9bc
hamborger <himanshu14121992@gmail.com>
2015-09-01 10:15:46 +0530

    E-E,E-I,I-E,I-I connections made

--- dev/propagation.py
+++ dev/propagation.py
@@ -4,7 +4,9 @@ Vr = -70 * mV
 Vt = -55 * mV
 taum = 10 * ms # was 10, 12.35 interesting TMM modifications 20140424
 taupsp = 0.325 * ms
-weight = 4.86 * mV
+weight_E = 4.86 * mV
+weight_I = -4.86 * mV
+
 # Neuron model
 eqs = Equations('''
 dV/dt=(-(V-Vr)+x)*(1./taum) : volt
@@ -12,29 +14,44 @@ dx/dt=(-x+y)*(1./taupsp) : volt
 dy/dt=-y*(1./taupsp)+25.27*mV/ms+\
     (39.24*mV/ms**0.5)*xi : volt
 ''')
+
 # Neuron groups.  Modified to be larger TMM
 P = NeuronGroup(N=1000, model=eqs,
     threshold=Vt, reset=Vr, refractory=1 * ms)
+
 # Input spike volley's a and sigma from user
 print "\nInput no.of neurons 'a' and temporal dispersion of spikes 'sigma'"
 user_n = int(raw_input("\na = "))
 user_sigma = float(raw_input("sigma(in mSec) = "))
 print "\n"
 Pinput = PulsePacket(t=10 * ms, n=user_n, sigma=user_sigma * ms)
+
 # The network structure.  Modified to be larger TMM
-Pgp = [ P.subgroup(100) for i in range(10)]
+Pgp_E = [ P.subgroup(88) for i in range(10)]
+Pgp_I = [ p.subgroup(12) for i in range(10)]
 C = Connection(P, P, 'y',delay=5*ms)
+
 # modified to be larger TMM
 for i in range(9):
-    C.connect_full(Pgp[i], Pgp[i + 1], weight)
-Cinput = Connection(Pinput, Pgp[0], 'y')
-Cinput.connect_full(weight=weight)
+    C.connect_full(Pgp_E[i], Pgp_E[i + 1], weight_E)
+    C.connect_full(Pgp_E[i], Pgp_I[i + 1], weight_E)
+    C.connect_full(Pgp_I[i], Pgp_E[i + 1], weight_I)
+    C.connect_full(Pgp_I[i], Pgp_I[i + 1], weight_I)
+    
+Cinput_E = Connection(Pinput, Pgp_E[0], 'y')
+Cinput_I = Connection(Pinput, Pgp_I[0], 'y')
+
+Cinput_E.connect_full(weight=weight_E)
+Cinput_I.connect_full(weight=weight_I)
+
 # Record the spikes
 Mgp = [SpikeMonitor(p) for p in Pgp]
 Minput = SpikeMonitor(Pinput)
 monitors = [Minput] + Mgp
+
 # Setup the network, and run it
 P.V = Vr + rand(len(P)) * (Vt - Vr)
+
 # Plot voltage trace
 trace = StateMonitor(P,'V',record=[1,101,201,301,401,501,601,701,801,999]) 
 
